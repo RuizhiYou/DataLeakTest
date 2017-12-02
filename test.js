261247f078c6928e81fa6a3391fa8a8a3178e2b8
You <hzyrz@icloud.com>
2017-12-02 17:14:59 -0500

    Test

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
+function addElement () { 
+  // create a new div element 
+  // and give it some content 
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode("Hi there and greetings!"); 
+  newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  alert("added")
+  document.body.insertBefore(newDiv, currentDiv); 
+}
+addElement()
+
 var a = navigator.oscpu
 console.log("=========");
-console.log(navigator.oscpu);
+alert("HEYEYEEYEYYE")
+alert(navigator.oscpu);
 console.log("=========");
